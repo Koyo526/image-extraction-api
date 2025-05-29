d855f2fa40d0b3750828a7b741a31a09d6f44f74
hamadayuuki <yuukirinnma@gmail.com>
2025-05-30 00:21:04 +0900

    Update request and response of Segment

--- app/routers/segment.py
+++ app/routers/segment.py
@@ -6,7 +6,7 @@ import logging
 
 from utils.segmentation import load_model, run_batch_segmentation
 from utils.file_io import save_results
-from models.models import Item, Result
+from models.models import Item, SegmentationResult
 
 # アプリ起動時にモデルをロード
 device, processor, model = load_model(use_cpu=False)
@@ -20,8 +20,24 @@ class SegmentRequest(BaseModel):
 
 router = APIRouter()
 
-@router.post("/segment", response_model=List[Result])
-def segment(request: SegmentRequest) -> List[Result]:
+
+"""
+- Request: SegmentRequest
+    セグメンテーション要求情報を表すデータクラス。
+    Attributes:
+        img_base64 (str): 全身画像のBase64エンコード文字列。
+        use_cpu (bool): CPUを強制使用する場合はTrue。デフォルトはFalse。
+
+
+- Response : SegmentationResult:
+    セグメンテーション処理結果を表すデータクラス。
+    Attributes:
+        tops_img_url (Optional[str]): トップスの画像URL。検出されなかった場合はNone。
+        bottoms_img_url (Optional[str]): ボトムスの画像URL。検出されなかった場合はNone。
+        runtime_sec (float): 推論に要した時間（秒）。
+"""
+@router.post("/segment", response_model=SegmentationResult)
+def segment(request: SegmentRequest) -> SegmentationResult:
     """
     画像リストに対してトップス/ボトムスのセグメンテーションを実行するエンドポイント。
 
@@ -32,7 +48,6 @@ def segment(request: SegmentRequest) -> List[Result]:
         List[Result]: セグメンテーション結果のリスト。
     """
     # セグメンテーション実行
-    results = run_batch_segmentation(request.img_base64, processor, model, request.out_dir)
-    # 結果をファイルに保存
-    save_results(results, Path("outputs/api_results.json"))
-    return results
+    segmentationResult = run_batch_segmentation(request.img_base64, processor, model, request.out_dir)
+    
+    return segmentationResult
